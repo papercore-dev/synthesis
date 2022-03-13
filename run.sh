52fe0ddade0a5bb95fe250d564719f622e6d799b
PaperCore <gyubini38@gmail.com>
2022-03-13 06:21:44 +0000

    환경 변수 설정

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+export TTS_MODEL_FILE=/home/ubuntu/synthesis/models/glowtts-v2/glow_model.pth.tar
+export TTS_MODEL_CONFIG=/home/ubuntu/synthesis/models/glowtts-v2/config.json
+export VOCODER_MODEL_ONYX=/home/ubuntu/synthesis/models/hifigan-v2/hifigan.onnx
 python3 -u server.py
